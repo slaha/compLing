4a710bcda47cc8841054bc78cd65e2ddeae1cea5
Jan Šlahora <jan@slahora.cz>
2014-10-18 23:36:44 +0200

    denotation graph characteristics

--- src/main/java/cz/compling/model/denotation/DenotationMath.java
+++ src/main/java/cz/compling/model/denotation/DenotationMath.java
@@ -32,4 +32,34 @@ public class DenotationMath {
 
 		return sum;
 	}
+
+	public double computeRelativeConnectionRate(int spikesCount, int componentsCount) {
+		if (spikesCount <= 1) {
+			throw new IllegalArgumentException("spikesCount cannot be <= 1");
+		}
+		if (componentsCount <= 0) {
+			throw new IllegalArgumentException("componentsCount cannot be <= 0");
+		}
+		double nominator = spikesCount - componentsCount;
+		double denominator = spikesCount - 1d;
+		return nominator / denominator;
+	}
+
+	public double computeRelativeCyclomaticNumber(int spikesCount, int componentsCount, int edgesCount) {
+		if (spikesCount <= 2) {
+			throw new IllegalArgumentException("spikesCount cannot be <= 2");
+		}
+		double nominator = 2d * ( edgesCount - spikesCount + componentsCount );
+		double denominator = (spikesCount - 1d) * (spikesCount - 2d);
+		return nominator / denominator;
+	}
+
+	public double computeConnotativeConcentration(int spikesCount, int edgesCount) {
+		if (spikesCount <= 1) {
+			throw new IllegalArgumentException("spikesCount cannot be <= 1");
+		}
+		double nominator = 2d * edgesCount;
+		double denominator = spikesCount * (spikesCount - 1d);
+		return nominator / denominator;
+	}
 }
